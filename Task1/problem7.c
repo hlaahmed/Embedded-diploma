fc19c7523565c2659c449f4b0cf630ecfef5e197
Hla Mansour <40806663+hlaahmed@users.noreply.github.com>
2020-08-20 17:50:10 +0200

    basics

---
+++ Task1/problem7.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int perfectsquare(int num1);//function that returns 1 if the number is a perfect square, and 0 otherwise
+int main()
+{
+	int num1;
+	printf("Enter the number\n");
+	scanf("%d",&num1);
+    if(perfectsquare(num1))
+	{
+		printf("the entered number is a perfect square");
+	}
+	else
+	{
+		printf("the entered number is Not a perfect square");
+	}
+	return 0;
+}
+int perfectsquare(int num1)
+{
+	for(int i= 1; i < num1 ; i++)	
+	{
+		if(i*i == num1)
+		{		
+			return 1;
+		}	
+	}
+	return 0;
+	
+}
+ 
+ 
